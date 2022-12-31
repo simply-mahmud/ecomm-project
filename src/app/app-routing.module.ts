8a7e860fcb6b52e389bff0714ebd0fb816fbf06f
DESKTOP-1JJ1H27\Mahmud CSE <simply.mahmud@gmail.com>
2022-12-31 09:41:44 +0600

    nav bar with header and seller form with angular binded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "seller-auth",
+    component: SellerAuthComponent
+  },
+  {
+    path: "**",
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
